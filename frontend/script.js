b660371364c8ddd849ca394b712900dac3b7bd8f
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2023-05-02 14:12:00 +0200

    implement checkSelectedCountry function

--- frontend/script.js
+++ frontend/script.js
@@ -6,10 +6,18 @@ function listCountriesIntoDropdown (data) {
   console.log(data);
 
   const selectElement = document.getElementById('all');
-  console.log(selectElement);
 
-  data.forEach((country) => {
+  const sorted = data.sort((a, b) => a.name.common - b.name.common);
+  sorted.forEach((country) => {
     selectElement.insertAdjacentHTML('beforeend', optionElement(country.name.common));
+  });
+}
+
+function checkSelectedCountry () {
+  const selectElement = document.getElementById('all');
+  const countryID = document.getElementById('country');
+  selectElement.addEventListener('change', function (event) {
+    countryID.textContent = `Kiválasztott ország: ${event.target.value}`;
   });
 }
 
@@ -17,5 +25,8 @@ function listCountriesIntoDropdown (data) {
 const loadEvent = function () {
   // Main
   listCountriesIntoDropdown(countries);
+  checkSelectedCountry();
+
+  
 };
 window.addEventListener('load', loadEvent);
